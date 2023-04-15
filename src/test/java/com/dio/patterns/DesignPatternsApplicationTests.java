63d0f4ce597efda885883427d9d06cbc52d35b5d
Matheus Miranda <matheus-miranda@users.noreply.github.com>
2023-04-15 00:18:59 -0300

    initial commit

---
+++ src/test/java/com/dio/patterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
