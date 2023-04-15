5167493abb68e87ecb675f704977b8130caeae8c
Matheus Miranda <matheus-miranda@users.noreply.github.com>
2023-04-15 01:21:37 -0300

    include feign

--- src/main/java/com/dio/patterns/DesignPatternsApplication.java
+++ src/main/java/com/dio/patterns/DesignPatternsApplication.java
@@ -2,7 +2,9 @@ package com.dio.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class DesignPatternsApplication {
 
